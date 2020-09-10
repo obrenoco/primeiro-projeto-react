d422dd79931432a18240a431fb2872ff3ca89707
Your Name <bsromeiro@gmail.com>
2020-09-10 17:26:02 -0300

    fix: Security issues on deploy

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -87,7 +87,7 @@ const Repository: React.FC = () => {
 
             <Issues>
                 {issues.map( issue => (
-                    <a key={issue.id} href={issue.html_url} target="_blank">
+                    <a key={issue.id} href={issue.html_url} target="_blank" rel="noopener noreferrer">
                         <div>
                             <strong>{issue.title}</strong>
                             <p>{issue.user.login}</p>
